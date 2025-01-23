8074e5093538dabc3c74932df35c73e21eb9a13a
campione <medsaif.mbarki@digidco.com>
2025-01-23 21:28:38 +0100

    ci: Docker build and push to Docker Hub

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,15 +1,12 @@
 const globalError = (err, req, res, next) => {
-  // Set default values for status code and status
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  // Trimmed environment variable check to ensure consistent matching
   const environment = process.env.NODE_ENV?.trim() || "production";
 
   if (environment === "development") {
     sendErrorForDev(err, res);
   } else {
-    // Handle production errors as a fallback
     sendErrorForProd(err, res);
   }
 };
@@ -25,7 +22,6 @@ const sendErrorForDev = (err, res) => {
 };
 
 const sendErrorForProd = (err, res) => {
-  // If the error is operational, provide a client-friendly response
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
@@ -33,7 +29,6 @@ const sendErrorForProd = (err, res) => {
       message: err.message,
     });
   } else {
-    // Log the detailed error for internal purposes and provide a generic response to clients
     console.error("ERROR 💥:", err);
     res.status(500).json({
       status: "error",
